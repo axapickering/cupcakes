07b89bc84e4af608278461f918985a3081e8dc75
Axa Pickering <axapickering@gmail.com>
2023-10-09 11:32:09 -0700

    created list all cupcakes route and Model

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
 """Flask app for Cupcakes"""
+
+import os
+
+from flask import Flask, request, redirect, render_template, flash, jsonify
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///cupcakes')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "nibblers"
+
+connect_db(app)
+
+@app.get("/api/cupcakes")
+def get_all_cupcakes():
+    ''' Get info on all cupcakes '''
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.get("/api/cupcakes/<int:cupcake_id>")
+def get_cupcake_info():
+    ''' Get info on a specific cupcake'''
+
+
+
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+    ''' Creates a new cupcake from request data '''
--- models.py
+++ models.py
@@ -1,1 +1,60 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://tinyurl.com/demo-cupcake"
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """Cupcake."""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    flavor = db.Column(
+        db.String(50),
+        nullable=False,
+        default=""
+    )
+
+    size = db.Column(
+        db.String(15),
+        nullable=False,
+        default=""
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(500),
+        nullable=False,
+        default=DEFAULT_IMAGE_URL
+    )
+
+    def serialize(self):
+        ''' Returns a serialized dictionary of the instance'''
+
+        return {
+            "id" : self.id,
+            "flavor" : self.flavor,
+            "size" : self.size,
+            "rating" : self.rating,
+            "image_url" : self.image_url
+        }
